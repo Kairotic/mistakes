bf11b101f1b23eff75490db9b8db83dc371a73e3
alex <lxyxpx@gmail.com>
2016-04-06 10:33:58 +0100

    - ‘.#Dyadic.hs’ / ‘Dyadic.hs’

--- Dyadic.hs
+++ Dyadic.hs
@@ -40,7 +40,7 @@ measure x y = do a <- measure' x y
                  b <- measure x a
                  return (AddUnit Unit b)
 
-measure' :: Multitude -> Multitude -> Maybe Multitude
+measure' :: Multitude a -> Multitude a -> Maybe (Multitude a)
 measure' [] x = Just x
 measure' x [] = Nothing
 measure' x y = measure' (tail x) (tail y)
