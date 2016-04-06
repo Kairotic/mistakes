679d2116140429d3c9d9525400c0b167a65139a8
alex <lxyxpx@gmail.com>
2016-04-06 10:35:30 +0100

    - ‘.#Dyadic.hs’ / ‘Dyadic.hs’

--- Dyadic.hs
+++ Dyadic.hs
@@ -45,7 +45,7 @@ measure x y = do a <- measure' x y
 measure' :: Multitude a -> Multitude a -> Maybe (Multitude a)
 measure' (Pair _ _) x = Just x
 measure' x (Pair _ _) = Nothing
-measure' x y = measure' (tail x) (tail y)
+measure' (AddUnit _ x) (AddUnit _ y) = measure' x y
 
 -- isPart l g = isJust (measure l g)
 
